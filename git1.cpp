03bee3225fc0e955939997bb653383e075197c8a
naner <1347808702@qq.com>
2023-11-02 21:07:10 +0800

    添加密码强度和密码生成功能

--- git1.cpp
+++ git1.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 // 定义一个加密函数，接受一个字符串参数，返回加密后的字符串
@@ -33,53 +35,123 @@ string decrypt(string s) {
     return s;
 }
 
-// 定义一个主函数，实现用户界面和交互逻辑
-int main() {
-    // 定义一个变量，用于存储用户选择的操作序号
-    int choice;
-    // 定义一个变量，用于存储用户输入或输出的密码
-    string password;
-    // 使用一个循环，让用户可以重复选择操作，直到选择退出为止
-    while (true) {
-        // 输出提示信息，让用户选择操作
-        cout << "请选择操作：" << endl;
-        cout << "1. 加密" << endl;
-        cout << "2. 解密" << endl;
-        cout << "5. 退出" << endl;
-        cout << "请输入选项序号：" << endl;
-        // 输入用户选择的操作序号
-        cin >> choice;
-        // 根据用户选择的操作序号，执行相应的功能
-        switch (choice) {
-        case 1: // 如果用户选择加密
-            // 输出提示信息，让用户输入密码
-            cout << "请输入要加密的密码（每个字符为数字或者大小写字母，长度不超过16）：" << endl;
-            // 输入用户输入的密码
-            cin >> password;
-            // 调用加密函数，对密码进行加密，并将结果赋值给password变量
-            password = encrypt(password);
-            // 输出提示信息，显示加密后的密码
-            cout << "加密后的密码为：" << password << endl;
-            break;
-        case 2: // 如果用户选择解密
-            // 输出提示信息，让用户输入密码
-            cout << "请输入要解密的密码（每个字符为数字或者大小写字母，长度不超过16）：" << endl;
-            // 输入用户输入的密码
-            cin >> password;
-            // 调用解密函数，对密码进行解密，并将结果赋值给password变量
-            password = decrypt(password);
-            // 输出提示信息，显示解密后的密码
-            cout << "解密后的密码为：" << password << endl;
-            break;
-        case 5: // 如果用户选择退出
-            // 输出提示信息，表示程序结束
-            cout << "感谢您的使用，再见！" << endl;
-            // 退出循环
-            return 0;
-        default: // 如果用户选择其他无效的序号
-            // 输出提示信息，表示输入错误
-            cout << "输入错误，请重新输入！" << endl;
+// 定义一个函数，接受一个字符串参数，返回该密码的强度等级
+string judgePasswordStrength(string s) {
+    int length = s.length();
+    bool hasDigit = false;
+    bool hasLower = false;
+    bool hasUpper = false;
+
+    // 遍历字符串中的每个字符
+    for (int i = 0; i < s.length(); i++) {
+        // 如果该字符是数字，并且之前没有出现过数字
+        if (isdigit(s[i]) && !hasDigit) {
+            // 将hasDigit设为true，表示已经出现过数字
+            hasDigit = true;
+        }
+        // 如果该字符是小写字母，并且之前没有出现过小写字母
+        if (islower(s[i]) && !hasLower) {
+            // 将hasLower设为true，表示已经出现过小写字母
+            hasLower = true;
+        }
+        // 如果该字符是大写字母，并且之前没有出现过大写字母
+        if (isupper(s[i]) && !hasUpper) {
+            // 将hasUpper设为true，表示已经出现过大写字母
+            hasUpper = true;
         }
     }
+
+    // 根据密码长度和字符类型判断密码强度等级，并返回相应的字符串
+    if (length < 8 || (length >= 8 && (hasDigit + hasLower + hasUpper) == 1)) {
+        return "弱强度";
+    }
+    else if (length >= 8 && (hasDigit + hasLower + hasUpper) == 2) {
+        return "中强度";
+    }
+    else if (length >= 8 && (hasDigit + hasLower + hasUpper) == 3) {
+        return "高强度";
+    }
+    else {
+        return "无效密码";
+    }
+}
+
+// 生成密码
+string generatePassword(int length) {
+    const string validChars = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    string generatedPassword;
+    for (int i = 0; i < length; i++) {
+        generatedPassword += validChars[rand() % validChars.length()];
+    }
+    return generatedPassword;
+}
+
+int main() {
+    srand(time(0));
+    int choice;
+    string password;
+
+    while (true) {
+        cout << "==============================\n";
+        cout << "欢迎使用密码管理系统\n";
+        cout << "==============================\n";
+        cout << "请选择操作：\n";
+        cout << "1. 加密\n";
+        cout << "2. 解密\n";
+        cout << "3. 判断密码强度\n";
+        cout << "4. 密码生成\n";
+        cout << "5. 退出\n";
+        cout << "请输入选项序号：";
+
+        cin >> choice;
+
+        switch (choice) {
+        case 1:
+            cout << "==============================\n";
+            cout << "请输入要加密的密码：" << endl;
+            cin >> password;
+            password = encrypt(password);
+            cout << "加密后的密码为：" << password << endl;
+            break;
+
+        case 2:
+            cout << "==============================\n";
+            cout << "请输入要解密的密码：" << endl;
+            cin >> password;
+            password = decrypt(password);
+            cout << "解密后的密码为：" << password << endl;
+            break;
+
+        case 3:
+            cout << "==============================\n";
+            cout << "请输入要判断强度的密码：" << endl;
+            cin >> password;
+            cout << "您输入的密码强度为：" << judgePasswordStrength(password) << endl;
+            break;
+
+        case 4:
+            int passwordLength;
+            cout << "==============================\n";
+            cout << "请输入密码长度：" << endl;
+            cin >> passwordLength;
+            if (passwordLength < 1 || passwordLength > 16) {
+                cout << "密码长度不符合要求，请重新输入！" << endl;
+            }
+            else {
+                string generatedPassword = generatePassword(passwordLength);
+                cout << "生成的密码为：" << generatedPassword << endl;
+            }
+            break;
+
+        case 5:
+            cout << "感谢您的使用，再见！" << endl;
+            return 0;
+
+        default:
+            cout << "输入错误，请重新输入！" << endl;
+            break;
+        }
+    }
+
     return 0;
 }
